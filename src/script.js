5dd2df5637b67de28bd4f050a52d836c7882ff9b
Cheburek254 <kirden06122007@gmail.com>
2024-03-13 18:40:46 +0300

    a

---
+++ src/script.js
@@ -0,0 +1,24 @@
+let btn_history = document.querySelector('.history')
+let btn_choose = document.querySelector('.choose')
+let btn_all = document.querySelector('.all')
+let history_block = document.querySelector('.history-for-js')
+let crafts_block = document.querySelector('.craft-to-js')
+let choose_block = document.querySelector('.choose-for-js')
+
+btn_history.addEventListener('click',function(){
+    history_block.style.display = 'flex'
+    crafts_block.style.display = 'none'
+    choose_block.style.display = 'none'
+})
+
+btn_all.addEventListener('click',function(){
+    crafts_block.style.display = 'flex'
+    history_block.style.display = 'none'
+    choose_block.style.display = 'none'
+})
+
+btn_choose.addEventListener('click',function(){
+    choose_block.style.display = 'flex'
+    crafts_block.style.display = 'none'
+    history_block.style.display = 'none'
+})
