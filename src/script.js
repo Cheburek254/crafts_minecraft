7e241a3c5d8ca2450a284de92c6733f5ac650c5b
Кирилл <105377400+Cheburek254@users.noreply.github.com>
2024-03-27 19:28:25 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -8,23 +8,23 @@ let crafts = document.querySelector('.crafts')
 //let receips = {
 //   название предмета :    [imageurl,[список необходимых предмктов(0  если в этой ячейке предмета нет)]]
 //}
-let receips = {"bubu" : ["img/craftable/tile004.png"],
-"bubu1" : ["img/craftable/tile004.png"],
-"bubu2" : ["img/craftable/tile004.png"],
-"bubu3" : ["img/craftable/tile004.png"],
-"bubu4" : ["img/craftable/tile004.png"],
-"bubu5" : ["img/craftable/tile004.png"],
-"bubu6" : ["img/craftable/tile004.png"],
-"bubu7" : ["img/craftable/tile004.png"],
-"bubu8" : ["img/craftable/tile004.png"],
-"bubu9" : ["img/craftable/tile004.png"],
-"bubu0" : ["img/craftable/tile004.png"],
-"bubu-" : ["img/craftable/tile004.png"],
-"bubu=" : ["img/craftable/tile004.png"],
-"bubu[" : ["img/craftable/tile004.png"],
-"bubup" : ["img/craftable/tile004.png"],
-"bubuo" : ["img/craftable/tile004.png"],
-"bubui" : ["img/craftable/tile004.png"],
+let receips = {"bubu" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu1" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu2" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu3" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu4" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu5" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu6" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu7" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu8" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu9" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu0" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu-" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu=" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubu[" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubup" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubuo" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
+"bubui" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]]
 }
 btn_history.addEventListener('click',function(){
     history_block.style.display = 'flex'
@@ -47,14 +47,37 @@ btn_choose.addEventListener('click',function(){
 let a = crafts.querySelector('.row')
 Object.keys(receips).forEach(key => {
     //console.log(key, receips[key])
-    
-    
-    
-    a.innerHTML+=`<div class="icon"><img class="block-img" src="${receips[key]}"></div>`
-    
-    
+     
+    a.innerHTML+=`<div class="icon spa"><img class="block-img" src="${receips[key][0]}"></div>`
         
-    
-    
-    
 })
+let items = document.querySelectorAll('.spa')
+let objects = Object.keys(receips)
+let open = document.querySelector('.open')
+console.log(objects,objects.length)
+for (let i = 0;i<objects.length;i++){
+    items[i].addEventListener('click',function(){
+        open.innerHTML = `<div class="left">
+        <div class="row1">
+          <img class="block-img" src="${receips[objects[i]][1][0]}" alt = "">
+          <img class="block-img" src="${receips[objects[i]][1][1]}" alt = "">
+          <img class="block-img" src="${receips[objects[i]][1][2]}" alt = "">
+        </div>
+        <div class="row1">
+          <img class="block-img" src="${receips[objects[i]][1][3]}" alt = "">
+          <img class="block-img" src="${receips[objects[i]][1][4]}" alt = "">
+          <img class="block-img" src="${receips[objects[i]][1][5]}" alt = "">
+        </div>
+        <div class="row1">
+          <img class="block-img" src="${receips[objects[i]][1][6]}" alt = "">
+          <img class="block-img" src="${receips[objects[i]][1][7]}" alt = "">
+          <img class="block-img" src="${receips[objects[i]][1][8]}" alt = "">
+        </div>
+      </div>
+      <div class="arrow"><img src="img/arrow.png"></div>
+      <div class="icon"><img class="block-img" src="${receips[objects[i]][0]}"></div>
+        `
+        console.log(i)
+
+    })
+}
